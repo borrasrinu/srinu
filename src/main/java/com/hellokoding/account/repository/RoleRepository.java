1209de32477857f478813411d740636cdf9789ca
srinu.borra <srinu.borra@apcfss.in>
2023-04-12 16:35:05 +0530

    sdgf

--- src/main/java/com/hellokoding/account/repository/RoleRepository.java
+++ src/main/java/com/hellokoding/account/repository/RoleRepository.java
@@ -4,4 +4,9 @@ import com.hellokoding.account.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long>{
+	
+	
+	
+	
+	
 }
